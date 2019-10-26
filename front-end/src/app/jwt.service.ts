eb4ebdd6841ec73a1ccd0aa9d7d692b2b179918c
AbdElrahim Mostafa <abdalrhim122mostafa@gmail.com>
2019-10-26 16:38:19 +0200

    make jwt service to heandel every thing about token

---
+++ front-end/src/app/jwt.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import * as jwt_decode from 'jwt-decode';
+@Injectable({
+  providedIn: 'root'
+})
+export class JwtService {
+
+  constructor() { }
+
+  getToken() : string {
+    return localStorage.getItem('token');
+  }
+
+  setToken(token) {
+    return localStorage.setItem('token' , token);
+  }
+  
+  resetToken() {
+    localStorage.removeItem('token');
+  }
+
+  isTokenExpire() : boolean {
+    let token = this.getToken();
+    if(token){
+      let decoded = jwt_decode(token);
+      const date = new Date(0); 
+      date.setUTCSeconds(decoded.exp);
+      return !(date.valueOf() > new Date().valueOf());
+    } else {
+      return true;
+    }
+  }
+
+}
