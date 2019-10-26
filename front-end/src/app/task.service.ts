9f3c11d1ea744a70518df16f4206b9ac9ddbeb17
AbdElrahim Mostafa <abdalrhim122mostafa@gmail.com>
2019-10-26 18:32:45 +0200

    make task service to heandel transtion of task data between front end and server

---
+++ front-end/src/app/task.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient , HttpErrorResponse , HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+// import { JwtService } from './jwt.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor(private http : HttpClient) { }
+
+  newAndUpdateTask(data : Object , path : string) : Observable<Object>{
+    let token = localStorage.getItem('token');
+    return this.http.post<Object>(`http://localhost:3000/task/${path}` , data , {
+      headers : new HttpHeaders({
+        'Content-Type' : 'application/json',
+        'Authorization' : `Bearer ${token}`
+      })
+    }).pipe(catchError(this.responsError));
+  }
+
+  listAllTasks() : Observable<Object>{
+    let token = localStorage.getItem('token');
+    return this.http.get<Object>(`http://localhost:3000/task/list` , {
+      headers : new HttpHeaders({
+        'Content-Type' : 'application/json',
+        'Authorization' : `Bearer ${token}`
+      })
+    }).pipe(catchError(this.responsError));
+  }
+
+  responsError(error : HttpErrorResponse) {
+    if(error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(`Backend returned code ${error.status}, body was: ${error.error}`);
+    }
+    return throwError(error);
+  }
+
+}
